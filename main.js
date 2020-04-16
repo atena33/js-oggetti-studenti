027d8478903c8fc84dd654f3dfb439215831f31f
Anna <annamura833@gmail.com>
2020-04-16 19:27:23 +0200

    Completo

--- main.js
+++ main.js
@@ -19,14 +19,18 @@ console.log(studente);
 // -Stampare a schermo attraverso il for in tutte le proprietà;
  for (var key in studente){
    console.log(studente[key]);
+   var stampa = studente[key];
+   var contenutoPrecedente = $('#stampa').html();
+   $('#stampa').text(' ' + contenutoPrecedente + ' ' +stampa);
  }
+
 
 // - Creare un array di oggetti di studenti.
 
 var studenti = [
   {
-    'Nome': 'Cristina',
-    'Cognome': 'Rossi',
+    'Nome': 'Arianna',
+    'Cognome': 'Sanna',
     'Eta': 25
   },
   {
@@ -43,18 +47,20 @@ var studenti = [
 
 // -Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 for (var i = 0; i < studenti.length; i++) {
-  var stampa = studenti[i];
-  var nome = stampa.Nome;
-  var cognome = stampa.Cognome;
+  var data = studenti[i];
+  var nome = data.Nome;
+  var cognome = data.Cognome;
   console.log(nome + " " + cognome);
+  var contenutoPrecedente = $('.studenti').html();
+  $('.studenti').text(contenutoPrecedente + ' ' + nome + ' ' + cognome);
 }
 
 //Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo
 //  oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-var nuovoNome = $(prompt('inserisci nome')).val();
-var nuovoCognome = $ (prompt('inserisci cognome')).val();
-var nuovaEta= $(prompt('inserisci età')).val();
+var nuovoNome = prompt('inserisci nome');
+var nuovoCognome = prompt('inserisci cognome');
+var nuovaEta= prompt('inserisci età');
 
 studenti.push(
   {
@@ -62,6 +68,16 @@ studenti.push(
     'Cognome': nuovoCognome,
     'Eta': nuovaEta
   }
-)
+);
 
 console.log(studenti);
+
+for (var i = 0; i < studenti.length; i++) {
+  var data = studenti[i];
+  var nome = data.Nome;
+  var cognome = data.Cognome;
+  var eta = data.Eta
+  // $('#stampa').append('<p class="nuovo">' nome + cognome + eta + '</p>')
+}
+
+$('.studenti').append(' Si è aggiunta/o: '+ nome + ' ' + cognome + ' ' + ' di anni ' + eta)
